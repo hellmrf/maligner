248f04eac3db23ca5315c9f2d161c67fc2ef3fa8
Heliton Martins <helitonmrf@gmail.com>
2024-03-07 19:01:37 -0300

    add docs

--- maligner/aligner.py
+++ maligner/aligner.py
@@ -5,6 +5,19 @@ from maligner.mtypes import Mol
 
 
 def find_MCS(mols: list[Mol], flexible=False) -> Mol:
+    """ Finds the Maximum Common Substructure (MCS) among a list of molecules.
+
+    Args:
+        mols (list[Mol]): A list of RDKit Mol objects representing the input molecules.
+        flexible (bool, optional): Specifies whether to allow flexible matching or not.
+            If set to True, matching structures with different atoms or with different 
+            bond types will be allowed. For example, a carbon atom will match with a
+            nitrogen atom, and a single bond will match with a double bond.
+            Defaults to False.
+
+    Returns:
+        Mol: The RDKit Mol object representing the MCS.
+    """
     if len(mols) < 2:
         raise ValueError("At least two molecules are required to find the MCS")
 
@@ -19,6 +32,20 @@ def find_MCS(mols: list[Mol], flexible=False) -> Mol:
 
 
 def get_MCS_atoms(mols: list[Mol], flexible=False) -> list[list[int]]:
+    """
+    Returns the atoms that correspond to the Maximum Common Substructure (MCS) in a list of molecules.
+
+    Args:
+        mols (list[Mol]): A list of RDKit Mol objects representing the molecules.
+        flexible (bool, optional): Whether to allow flexible matching of the MCS. 
+            See `maligner.aligner.find_MCS` for more info. Defaults to False.
+
+    Returns:
+        list[list[int]]: A list of lists, where each inner list contains the atom indices that correspond to the MCS in each molecule.
+
+    Raises:
+        ValueError: If less than two molecules are provided.
+    """
     if len(mols) < 2:
         raise ValueError("At least two molecules are required to find the MCS")
 
