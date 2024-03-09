c96e09e52a1beaa2790b0c8085572d9e35fa9a2d
Heliton Martins <helitonmrf@gmail.com>
2024-03-09 14:19:05 -0300

    update translations

--- maligner/translations/pt_BR.ts
+++ maligner/translations/pt_BR.ts
@@ -1,51 +1,46 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="pt_BR">
+<TS version="2.1" language="pt">
 <context>
     <name>MainWindow</name>
     <message>
-        <location filename="../mainwindow.py" line="64"/>
+        <location filename="../mainwindow.py" line="34"/>
+        <source>maligner |\ An Open-Source Molecular Alignment Tool</source>
+        <translation>maligner |\ Alinhamento Molecular</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.py" line="60"/>
         <source>File</source>
         <translation>Arquivo</translation>
     </message>
     <message>
-        <location filename="../mainwindow.py" line="65"/>
+        <location filename="../mainwindow.py" line="67"/>
         <source>Tools</source>
         <translation>Ferramentas</translation>
     </message>
     <message>
-        <location filename="../mainwindow.py" line="66"/>
+        <location filename="../mainwindow.py" line="74"/>
         <source>Help</source>
         <translation>Ajuda</translation>
     </message>
     <message>
-        <location filename="../mainwindow.py" line="104"/>
-        <source>File opened</source>
-        <translation>Arquivo aberto</translation>
-    </message>
-    <message>
-        <location filename="../mainwindow.py" line="123"/>
-        <source>File saved</source>
-        <translation>Arquivo salvo</translation>
-    </message>
-    <message>
-        <location filename="../mainwindow.py" line="128"/>
-        <location filename="../mainwindow.py" line="158"/>
+        <location filename="../mainwindow.py" line="97"/>
+        <location filename="../mainwindow.py" line="122"/>
         <source>No molecule selected</source>
         <translation>Nenhuma molécula selecionada</translation>
     </message>
     <message>
-        <location filename="../mainwindow.py" line="136"/>
+        <location filename="../mainwindow.py" line="105"/>
         <source>Molecules removed</source>
         <translation>Moléculas removidas</translation>
     </message>
     <message>
-        <location filename="../mainwindow.py" line="150"/>
+        <location filename="../mainwindow.py" line="113"/>
         <source>About maligner</source>
         <translation>Sobre o maligner</translation>
     </message>
     <message>
-        <location filename="../mainwindow.py" line="153"/>
+        <location filename="../mainwindow.py" line="116"/>
         <source>maligner is an Open-Source Molecular Alignment Tool.
 
 
@@ -55,149 +50,104 @@ Some icons from: http://icons8.com
 Source code: https://github.com/hellmrf/maligner
 
 Released under GPL-v3.0.</source>
-        <translation>maligner é uma ferramenta de alinhamento molecular de código aberto.
+        <translation>maligner é uma ferramenta para alinhamento molecular brasileira e de código aberto, desenvolvida no Departamento de Química da UFMG.
 
 
-Baseado no RDKit: http://www.rdkit.org/
-Inspirado pelo rdeditor: https://github.com/EBjerrum/rdeditor
-Alguns ícones de: http://icons8.com
+Baseado no RDKit: https://www.rdkit.org/
+Baseado no rdeditor: https://github.com/EBjerrum/rdeditor
 Código-fonte: https://github.com/hellmrf/maligner
 
-Disponibilizado sob a licença GPL-v3.0.</translation>
+Distribuído sob licença GPL v3.0.</translation>
     </message>
     <message>
-        <location filename="../mainwindow.py" line="185"/>
+        <location filename="../mainwindow.py" line="138"/>
         <source>Open</source>
         <translation>Abrir</translation>
     </message>
     <message>
-        <location filename="../mainwindow.py" line="192"/>
+        <location filename="../mainwindow.py" line="140"/>
+        <source>Open an existing file</source>
+        <translation>Abrir arquivos existentes</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.py" line="143"/>
         <source>Save</source>
         <translation>Salvar</translation>
     </message>
     <message>
-        <location filename="../mainwindow.py" line="199"/>
-        <source>Exit</source>
-        <translation>Sair</translation>
-    </message>
-    <message>
-        <location filename="../mainwindow.py" line="206"/>
-        <source>About</source>
-        <translation>Sobre</translation>
-    </message>
-    <message>
-        <location filename="../mainwindow.py" line="211"/>
-        <source>About Qt</source>
-        <translation>Sobre o Qt</translation>
-    </message>
-    <message>
-        <location filename="../mainwindow.py" line="219"/>
-        <source>Delete</source>
-        <translation>Remover</translation>
-    </message>
-    <message>
-        <location filename="../mainwindow.py" line="228"/>
-        <source>Anchor current molecule</source>
-        <translation>Ancorar molécula</translation>
-    </message>
-    <message>
-        <source>O&amp;pen</source>
-        <translation type="vanished">Abrir &amp;O</translation>
-    </message>
-    <message>
-        <location filename="../mainwindow.py" line="188"/>
-        <source>Open an existing file</source>
-        <translation>Abrir arquivo existente</translation>
-    </message>
-    <message>
-        <source>S&amp;ave</source>
-        <translation type="vanished">Salvae</translation>
-    </message>
-    <message>
-        <location filename="../mainwindow.py" line="195"/>
+        <location filename="../mainwindow.py" line="146"/>
         <source>Save file</source>
         <translation>Salvar arquivo</translation>
     </message>
     <message>
-        <source>E&amp;xit</source>
-        <translation type="obsolete">Sair</translation>
+        <location filename="../mainwindow.py" line="149"/>
+        <source>Exit</source>
+        <translation>Sair</translation>
     </message>
     <message>
-        <location filename="../mainwindow.py" line="202"/>
+        <location filename="../mainwindow.py" line="151"/>
         <source>Exit the Application</source>
         <translation>Sair da aplicação</translation>
     </message>
     <message>
-        <source>A&amp;bout</source>
-        <translation type="obsolete">Sobre</translation>
+        <location filename="../mainwindow.py" line="154"/>
+        <source>About</source>
+        <translation>Sobre</translation>
     </message>
     <message>
-        <location filename="../mainwindow.py" line="208"/>
-        <source>Displays info about text editor</source>
+        <location filename="../mainwindow.py" line="155"/>
+        <source>Displays info about maligner</source>
+        <translation>Mostra informações sobre o maligner</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.py" line="158"/>
+        <source>About Qt</source>
+        <translation>Sobre o Qt</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.py" line="159"/>
+        <source>Show the Qt library&apos;s About box</source>
+        <translation>Mostra a caixa de informação sobre o Qt</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.py" line="163"/>
+        <source>Delete</source>
+        <translation>Deletar</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.py" line="165"/>
+        <source>Remove this molecule from canvas</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <source>About &amp;Qt</source>
-        <translation type="obsolete">Sobre o Qt</translation>
+        <location filename="../mainwindow.py" line="170"/>
+        <source>Anchor current molecule</source>
+        <translation>Ancorar molécula selecionada</translation>
     </message>
     <message>
-        <location filename="../mainwindow.py" line="213"/>
-        <source>Show the Qt library&apos;s About box</source>
-        <translation>Mostrar caixa de ajuda do Qt</translation>
-    </message>
-    <message>
-        <source>Delete &amp;X</source>
-        <translation type="obsolete">Deletar</translation>
-    </message>
-    <message>
-        <location filename="../mainwindow.py" line="222"/>
-        <source>Remove this molecule from canvas Ctrl+X</source>
-        <translation type="unfinished">Remover esta molécula da tela</translation>
-    </message>
-    <message>
-        <source>Anchor current molecule &amp;A</source>
-        <translation type="obsolete">Ancorar molécula</translation>
-    </message>
-    <message>
-        <location filename="../mainwindow.py" line="231"/>
+        <location filename="../mainwindow.py" line="175"/>
         <source>Set the selected molecule as the anchor for the alignment. (A)</source>
-        <translation>Definir a molécula selecionada como âncora para o alinhamento (A)</translation>
+        <translation>Ancorar molécula selecionada</translation>
     </message>
     <message>
-        <source>Open Selector</source>
-        <translation type="obsolete">Abrir seletor</translation>
-    </message>
-    <message>
-        <source>Opens the molecule selector for some molecule</source>
-        <translation type="obsolete">Abre o seletor de subestruturas para alguma molécula</translation>
-    </message>
-    <message>
-        <location filename="../mainwindow.py" line="245"/>
+        <location filename="../mainwindow.py" line="184"/>
         <source>Compute MCS</source>
-        <translation type="unfinished">Calcular MSC</translation>
+        <translation>Calcular MSC</translation>
     </message>
     <message>
-        <location filename="../mainwindow.py" line="249"/>
+        <location filename="../mainwindow.py" line="188"/>
         <source>Computes and select the Maximum Common Substructure for the loaded molecules.</source>
-        <translation>Calcular e selecionar a Máxima Subestrutura Comum para as moléculas abertas.</translation>
+        <translation>Calcula e seleciona a Máxima Subestrutura Comum (MSC) das moléculas abertas.</translation>
     </message>
     <message>
-        <location filename="../mainwindow.py" line="254"/>
+        <location filename="../mainwindow.py" line="192"/>
         <source>Run Alignment</source>
-        <translation>Alinhar</translation>
+        <translation>Rodar alinhamento</translation>
     </message>
     <message>
-        <location filename="../mainwindow.py" line="257"/>
+        <location filename="../mainwindow.py" line="194"/>
         <source>Align all the molecules.</source>
-        <translation>Alinha todas as moléculas.</translation>
-    </message>
-</context>
-<context>
-    <name>MolGridViewWidget</name>
-    <message>
-        <location filename="../widgets/MolGridView.py" line="81"/>
-        <source>Choose molecules</source>
-        <translation type="unfinished">Escolher moléculas</translation>
+        <translation>Alinhar todas as moléculas.</translation>
     </message>
 </context>
 </TS>
